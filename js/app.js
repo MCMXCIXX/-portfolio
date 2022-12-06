9adf20a00b1b714bb8d20709db0b5ccbf1aab92e
Valerii <shepotken2012@gmail.com>
2022-12-06 15:23:36 +0300

    исправил баг на маленьких экранах

--- js/app.js
+++ js/app.js
@@ -5,7 +5,21 @@ document.querySelectorAll('.slider').forEach((n, i) => {
 		direction: 'vertical',
 		mousewheel: true,
 		slidesPerView: 1.75,
-		slidesOffsetBefore: -125
+		slidesOffsetBefore: -125,
+		breakpoints: {
+			// when window width is >= 320px
+			299: {
+				height: 260,
+			},
+			// when window width is >= 480px
+			480: {
+				autoHeight: true,
+			},
+			// when window width is >= 640px
+			640: {
+				autoHeight: true,
+			}
+		  }
 	})
 })
 bindSwipers(slider1, slider2, slider3, slider4)
